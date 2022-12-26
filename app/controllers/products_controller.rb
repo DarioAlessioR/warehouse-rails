841621399d1ee67b8cf01d2c644d07e893d9ae26
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-12-26 16:45:30 -0400

    Create index.xlsx.axlsx file in products view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,12 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.xlsx {
+        response.headers['Content-Disposition'] = 'attachment; filename="products.xlsx"'
+      }
+    end
   end
 
   # GET /products/1 or /products/1.json
